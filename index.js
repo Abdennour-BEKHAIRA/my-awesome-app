709bfbaddfd38e814fbd2c43552a258554701d75
seddik@lablabee.com <seddik@lablabee.com>
2025-12-18 20:28:01 +0100

    Initial commit: animated app

---
+++ index.js
@@ -0,0 +1,13 @@
+﻿const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: "Hello from your animated app!" });
+});
+
+app.listen(port, () => {
+    console.log(Server running on port );
+});
